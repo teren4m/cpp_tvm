571aee5727b22838f5321cb91ed0481a9cc5d42f
Tim Andriyaschenko <timandriyaschenko@gmail.com>
2023-07-25 09:36:31 +0300

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,14 @@
 #include <opencv2/opencv.hpp>
 #include <chrono>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_syswm.h>
 
 using namespace cv;
 using namespace std::chrono;
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-// Starts up SDL and creates window
-bool init();
-
-// Loads media
-bool loadMedia();
-
-// Frees media and shuts down SDL
-void close();
-
-// The window we'll be rendering to
-SDL_Window *gWindow = NULL;
-
-// The surface contained by the window
-SDL_Surface *gScreenSurface = NULL;
-
-// The image we will load and show on the screen
-SDL_Surface *gHelloWorld = NULL;
+SDL_Window *window = NULL;
+const int WIDTH = 640;
+const int HEIGHT = 480;
 
 int main(int argc, char **argv)
 {
@@ -36,42 +20,41 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // Start up SDL and create window
-    if (!init())
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        printf("Failed to initialize!\n");
+        printf("SDL could not initialize! SDL_Error: %s\n",
+               SDL_GetError());
     }
     else
     {
-        // Load media
-        if (!loadMedia())
+        // Create a window
+        window = SDL_CreateWindow("BGFX Tutorial",
+                                  WIDTH, HEIGHT,
+                                  SDL_WINDOW_VULKAN);
+        if (window == NULL)
         {
-            printf("Failed to load media!\n");
+            printf("Window could not be created! SDL_Error: %s\n",
+                   SDL_GetError());
         }
-        else
+    }
+
+    bool quit = false;
+    SDL_Event currentEvent;
+    while (!quit)
+    {
+        while (SDL_PollEvent(&currentEvent) != 0)
         {
-            // Apply the image
-            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-
-            // Update the surface
-            SDL_UpdateWindowSurface(gWindow);
-
-            // Hack to get window to stay up
-            SDL_Event e;
-            bool quit = false;
-            while (quit == false)
+            if (currentEvent.type == SDL_EVENT_QUIT)
             {
-                while (SDL_PollEvent(&e))
-                {
-                    if (e.type == SDL_EVENT_QUIT)
-                        quit = true;
-                }
+                quit = true;
             }
         }
     }
 
-    // Free resources and close SDL
-    close();
+    // Free up window
+    SDL_DestroyWindow(window);
+    // Shutdown SDL
+    SDL_Quit();
 
     return 0;
 
@@ -94,64 +77,4 @@ int main(int argc, char **argv)
     //         break;
     // }
     return 0;
-}
-
-bool init()
-{
-    // Initialization flag
-    bool success = true;
-
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        success = false;
-    }
-    else
-    {
-        // Create window
-        gWindow = SDL_CreateWindow("SDL Tutorial", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_VULKAN);
-        if (gWindow == NULL)
-        {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-            success = false;
-        }
-        else
-        {
-            // Get window surface
-            gScreenSurface = SDL_GetWindowSurface(gWindow);
-        }
-    }
-
-    return success;
-}
-
-bool loadMedia()
-{
-    // Loading success flag
-    bool success = true;
-
-    // Load splash image
-    gHelloWorld = SDL_LoadBMP("hello_world.bmp");
-    if (gHelloWorld == NULL)
-    {
-        printf("Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError());
-        success = false;
-    }
-
-    return success;
-}
-
-void close()
-{
-    // Deallocate surface
-    SDL_DestroySurface(gHelloWorld);
-    gHelloWorld = NULL;
-
-    // Destroy window
-     SDL_DestroyWindow( gWindow );
-     gWindow = NULL;
-
-    // Quit SDL subsystems
-     SDL_Quit();
 }
